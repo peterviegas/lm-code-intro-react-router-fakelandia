51b341e5dd7f2b720111e3dc307cd3a14d1f8906
Peter Viegas <peterviegas@outlook.com>
2023-02-23 16:50:15 +0000

    Modified component router - correct navigation routesand cleanup of unused lines in App.tsx component

--- client/src/components/main_layout.tsx
+++ client/src/components/main_layout.tsx
@@ -3,12 +3,9 @@ import Footer from './footer';
 import { Outlet } from 'react-router-dom';
 
 export const MainLayout: React.FC = () => <>
-
-	<body>
-        <Header />
-		<main className='p-6' style={{flex: '1'}}>
-        	<Outlet />
-		</main>
-        <Footer />
-      </body>
+	<Header />
+	<main className='p-6' style={{flex: '1'}}>
+		<Outlet />
+	</main>
+	<Footer />
 </>;
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -5,6 +5,9 @@ import Home from "./components/home";
 import Footer from './components/footer';
 import NotFound  from "./components/notFound";
 import { MainLayout } from './components/main_layout';
+import Department from './components/department';
+import Justice from './components/justice';
+import Fakelandia from './components/fakelandia';
 
 export const Router : React.FC = () => <>
 	<Routes>
@@ -12,6 +15,9 @@ export const Router : React.FC = () => <>
 			<Route index element={<Home/>}/>
 			<Route path='about' element={<About/>}/>
 			<Route path='footer' element={<Footer/>}/>
+			<Route path='fakelandia' element={<Fakelandia/>}/>
+			<Route path='justice' element={<Justice/>}/>
+			<Route path='department' element={<Department/>}/>
 			<Route path='*' element={<NotFound/>}/>
 		</Route>
 	</Routes>

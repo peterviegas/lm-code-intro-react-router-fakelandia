d62f11069ae2112395348df85083c88b61005e2d
Peter Viegas <peterviegas@outlook.com>
2023-02-25 21:15:17 +0000

    Create router for confesstous

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -5,9 +5,10 @@ import Home from "./components/home";
 import Footer from './components/footer';
 import NotFound  from "./components/notFound";
 import { MainLayout } from './components/main_layout';
-import Department from './components/department';
 import Justice from './components/justice';
 import Fakelandia from './components/fakelandia';
+import Midemeanours from './components/misdemeanours';
+import ConfessToUs from './components/confess-to-us';
 
 export const Router : React.FC = () => <>
 	<Routes>
@@ -17,7 +18,8 @@ export const Router : React.FC = () => <>
 			<Route path='footer' element={<Footer/>}/>
 			<Route path='fakelandia' element={<Fakelandia/>}/>
 			<Route path='justice' element={<Justice/>}/>
-			<Route path='department' element={<Department/>}/>
+			<Route path='midemeanours' element={<Midemeanours/>}/>
+			<Route path='confess-to-us' element={<ConfessToUs/>}/>
 			<Route path='*' element={<NotFound/>}/>
 		</Route>
 	</Routes>
